39b8786465a36e99269e08cca0780789ef079c5a
omercakir2 <omercakir2323@gmail.com>
2023-08-14 14:00:12 +0300

    URI instead of URL

--- ecommerce/__init__.py
+++ ecommerce/__init__.py
@@ -4,7 +4,7 @@ db = SQLAlchemy()
 
 def createApp():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URL']='postgresql://postgres:postgres@localhost:5432/ecommerce'
+    app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:postgres@localhost:5432/ecommerce'
     #app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
 
     db.init_app(app)
